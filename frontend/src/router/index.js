5675fa370cfc3654c2bfe379cd0c3db40981a29f
Dwinanda <50469720+Dwinanda@users.noreply.github.com>
2024-04-22 13:40:27 +0700

    test

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import { VueRouter } from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  }
+]
+
+const router = new VueRouter([
+  routes
+])
+
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'Home',
+//       component: Home
+//     },
+//     {
+//       path: '/about',
+//       name: 'About',
+//       component: About
+//     }
+//   ]
+// })
